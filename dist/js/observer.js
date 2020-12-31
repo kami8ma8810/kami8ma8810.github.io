0d6b46d674167c60fd636ff62f2cfa9b4ca5eb63
Hayato Kamiyama <kami8ma8810@gmail.com>
2020-12-31 11:46:31 +0900

    portfolio2020とgulp4から統合。revealは使わない方向で

---
+++ dist/js/observer.js
@@ -0,0 +1,33 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var options = {
+    root: null,
+    rootMargin: '-50% 0px'
+  }; //監視する要素を指定
+
+  var observerElements = document.querySelectorAll('.js-trigger');
+  var observerFunc = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      // 動かす要素を指定
+      var targetElement = entry.target.querySelectorAll('.js-fadein');
+
+      if (entry.isIntersecting) {
+        gsap.fromTo(targetElement, {
+          y: 50
+        }, {
+          duration: 1,
+          opacity: 1,
+          rotation: '-360deg',
+          autoAlpha: 1,
+          y: 0,
+          stagger: .2
+        });
+        observerFunc.unobserve(entry.target);
+      }
+    });
+  }, options);
+  observerElements.forEach(function (el) {
+    observerFunc.observe(el);
+  });
+}, false);
---
+++ src/js/observer.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const options = {
+		root: null,
+		rootMargin: '-50% 0px',
+	};
+	//監視する要素を指定
+	const observerElements = document.querySelectorAll('.js-trigger');
+
+	const observerFunc = new IntersectionObserver(
+		(entries) => {
+			entries.forEach(entry => {
+				// 動かす要素を指定
+				const targetElement = entry.target.querySelectorAll('.js-fadein')
+
+				if (entry.isIntersecting) {
+					gsap.fromTo(targetElement, {
+						y: 50
+					}, {
+						duration: 1,
+						opacity: 1,
+						rotation: '-360deg',
+						autoAlpha: 1,
+						y: 0,
+						stagger: .2,
+					})
+					observerFunc.unobserve(entry.target);
+				}
+			});
+		},
+		options
+	);
+
+	observerElements.forEach(el => {
+		observerFunc.observe(el);
+	});
+}, false);
