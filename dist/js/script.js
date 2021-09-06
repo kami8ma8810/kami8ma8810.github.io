6c7033e4f04c5ffb22837fe0e8e2bbd86c325f21
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2021-09-06 17:11:13 +0900

    shuffleletters

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,18 +3,18 @@
 //MV100vh
 var setViewHeight = function setViewHeight() {
   var vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+  document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
 }; //リサイズ時にも発動するようにする
 
 
-window.addEventListener("resize", setViewHeight); //初期化
+window.addEventListener('resize', setViewHeight); //初期化
 
 setViewHeight(); // 現在のナビゲーションハイライト
 
 $(function () {
-  $(".header-nav a").each(function () {
+  $('.header-nav a').each(function () {
     if (this.href == location.href) {
-      $(this).parents("li").addClass("is-current");
+      $(this).parents('li').addClass('is-current');
     }
   });
 });
